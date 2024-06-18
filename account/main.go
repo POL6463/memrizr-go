af06fbfb9060d441adbf552c2ec96fbc6f084344
Kevin Kim <kevinkim0617kim@gmail.com>
2024-06-19 01:44:30 +0900

    Setting up gin server

--- account/main.go
+++ account/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/net/context"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	log.Println("Starting server...")
+
+	router := gin.Default()
+
+	router.GET("/api/account", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"hello": "world",
+		})
+	})
+
+	srv := &http.Server{
+		Addr: ":8080",
+		Handler: router,
+	}
+
+	// Gracefun server shutdown
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to initialize server: %v\n", err)
+		}
+	}()
+
+	log.Printf("Listening on port %v\n", srv.Addr)
+
+	// Wait for kill signal of channel
+	quit := make(chan os.Signal)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	log.Println("Shutting down server...")
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v\n", err)
+	}
 }
